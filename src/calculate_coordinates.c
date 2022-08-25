ab0b851694af3d11576bfe9200146ae45a486088
Bas Uitermark <88148607+BasUitermark@users.noreply.github.com>
2022-08-25 16:19:25 +0200

    removed rotation

--- src/calculate_coordinates.c
+++ src/calculate_coordinates.c
@@ -6,44 +6,44 @@
 /*   By: buiterma <buiterma@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/03/30 16:02:16 by buiterma      #+#    #+#                 */
-/*   Updated: 2022/07/07 12:42:28 by buiterma      ########   odam.nl         */
+/*   Updated: 2022/08/25 16:17:21 by buiterma      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 
-static void	x_rotation(int *y, int *z, double x_rot)
-{
-	int	temp_y;
-	int	temp_z;
+// static void	x_rotation(int *y, int *z, double x_rot)
+// {
+// 	int	temp_y;
+// 	int	temp_z;
 
-	temp_y = *y;
-	temp_z = *z;
-	*y = temp_y * cos(x_rot) + temp_z * sin(x_rot);
-	*z = -temp_y * sin(x_rot) + temp_z * cos(x_rot);
-}
+// 	temp_y = *y;
+// 	temp_z = *z;
+// 	*y = temp_y * cos(x_rot) + temp_z * sin(x_rot);
+// 	*z = -temp_y * sin(x_rot) + temp_z * cos(x_rot);
+// }
 
-static void	y_rotation(int *x, int *z, double y_rot)
-{
-	int	temp_x;
-	int	temp_z;
+// static void	y_rotation(int *x, int *z, double y_rot)
+// {
+// 	int	temp_x;
+// 	int	temp_z;
 
-	temp_x = *x;
-	temp_z = *z;
-	*x = temp_x * cos(y_rot) + temp_z * sin(y_rot);
-	*z = -temp_x * sin(y_rot) + temp_z * cos(y_rot);
-}
+// 	temp_x = *x;
+// 	temp_z = *z;
+// 	*x = temp_x * cos(y_rot) + temp_z * sin(y_rot);
+// 	*z = -temp_x * sin(y_rot) + temp_z * cos(y_rot);
+// }
 
-static void	z_rotation(int *x, int *y, double z_rot)
-{
-	int	temp_x;
-	int	temp_y;
+// static void	z_rotation(int *x, int *y, double z_rot)
+// {
+// 	int	temp_x;
+// 	int	temp_y;
 
-	temp_x = *x;
-	temp_y = *y;
-	*x = temp_x * cos(z_rot) - temp_y * sin(z_rot);
-	*y = temp_x * sin(z_rot) + temp_y * cos(z_rot);
-}
+// 	temp_x = *x;
+// 	temp_y = *y;
+// 	*x = temp_x * cos(z_rot) - temp_y * sin(z_rot);
+// 	*y = temp_x * sin(z_rot) + temp_y * cos(z_rot);
+// }
 
 static void	iso_rotation(int *x, int *y)
 {
@@ -60,11 +60,11 @@ t_coord	calc_coord(t_instance fdf, int x, int y, int z)
 {
 	x *= fdf.grid.size;
 	y *= fdf.grid.size;
-	x_rotation(&y, &z, fdf.grid.x_rot);
-	y_rotation(&x, &z, fdf.grid.y_rot);
-	z_rotation(&x, &y, fdf.grid.z_rot);
+	// x_rotation(&y, &z, fdf.grid.x_rot);
+	// y_rotation(&x, &z, fdf.grid.y_rot);
+	// z_rotation(&x, &y, fdf.grid.z_rot);
 	iso_rotation(&x, &y);
-	x += WIDTH / 2 - fdf.grid.x_offset;
-	y += -z + HEIGHT / 2 - fdf.grid.y_offset;
+	x += WIDTH / 2;
+	y += -z + HEIGHT / 2;
 	return (translate_coord(x, y));
 }
